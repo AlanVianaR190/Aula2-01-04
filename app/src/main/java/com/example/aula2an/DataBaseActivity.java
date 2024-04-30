f3a9c1773aca1a449987ae0e99d01e9665125a2b
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-04-29 21:11:54 -0300

    add

--- app/src/main/java/com/example/aula2an/DataBaseActivity.java
+++ app/src/main/java/com/example/aula2an/DataBaseActivity.java
@@ -3,6 +3,7 @@ package com.example.aula2an;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
 
@@ -10,53 +11,66 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DataBaseActivity extends AppCompatActivity {
+public class DatabaseActivity extends AppCompatActivity {
+
 
     private EditText editTextRe;
     private EditText editTextNome;
-    private EditText editTextDataAdmissao;
+    private EditText editTextDatadeAdm;
     private EditText editTextSalario;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_base2);
-
         editTextRe = findViewById(R.id.editTextRE);
         editTextNome = findViewById(R.id.editTextNome);
-        editTextDataAdmissao = findViewById(R.id.editTextDataAdmisao);
+        editTextDatadeAdm = findViewById(R.id.editTextDataAdmissao);
         editTextSalario = findViewById(R.id.editTextSalario);
 
     }
 
     private void limpar(){
-
         editTextRe.getText().clear();
         editTextNome.getText().clear();
-        editTextDataAdmissao.getText().clear();
+        editTextDatadeAdm.getText().clear();
         editTextSalario.getText().clear();
     }
 
-    public void cadastrar(View view) {
 
+    public void cadastrar(View view){
         AppDatabase db = AppDatabase.getInstance(this);
         FuncionarioDao dao = db.funcionarioDao();
-
-        int re = Integer.parseInt(editTextRe.getText().toString());
+        int RE = Integer.parseInt(editTextRe.getText().toString());
         String nome = editTextNome.getText().toString();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date dataAdmissao;
-        try{
-            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
-        }catch (ParseException r) {
+        try {
+            dataAdmissao = dateFormat.parse(editTextDatadeAdm.getText().toString());
+        } catch (ParseException r ) {
             dataAdmissao = new Date();
         }
-
         double salario = Double.parseDouble(editTextSalario.getText().toString());
-        Funcionario f = new Funcionario(re, nome, dataAdmissao, salario);
-        dao.insert(f);
+        Funcionario f = new Funcionario(RE, nome, dataAdmissao, salario);
+        if (view.getId() == R.id.button3){
+            dao.insert(f);
+        } else if (view.getId() == R.id.button4) {
+            dao.update(f);
+        } else {
+            dao.delete(f);
+        }
         limpar();
+    }
+
+    public void buscar (View view){
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re =Integer.parseInt(editTextRe.getText().toString());
+        Funcionario f = dao.buscarPeloRe(re);
+        editTextNome.setText(f.getNome());
+        editTextDatadeAdm.setText(dateFormat.format(f.getDataAdmissao()));
+        editTextSalario.setText(Double.toString(f.getSalario()));
 
     }
 }
