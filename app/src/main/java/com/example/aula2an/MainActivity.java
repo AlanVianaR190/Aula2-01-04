c1143167da55d18d1d48a847c59f0987f1978361
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-05-13 20:39:08 -0300

    add

--- app/src/main/java/com/example/aula2an/MainActivity.java
+++ app/src/main/java/com/example/aula2an/MainActivity.java
@@ -20,7 +20,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void abrirDataBase(View view) {
-        Intent intent = new Intent(this, DataBaseActivity.class);
+        Intent intent = new Intent(this, DatabaseActivity.class);
+        startActivity(intent);
+    }
+
+    public void abrirDesenho(View view) {
+        Intent intent = new Intent(this, DesenhoActivity.class);
         startActivity(intent);
     }
 }
